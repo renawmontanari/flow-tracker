6b5987e111333a28f46823ca07350593ea6bff1f
renawmontanari <regiangelica@gmail.com>
2025-05-08 20:03:09 -0300

    crinado os componentes barra lateral e formulario

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default {
+  name: "BarraLateral",
+};
+</script>
+
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="logo" />
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #ffffff;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
