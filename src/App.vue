74ffe467cd9c39d418471bbc2ab474972fa78bfa
renawmontanari <regiangelica@gmail.com>
2025-05-08 20:18:52 -0300

    crinado os componentes formulario

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/formulario.vue";
 
 export default {
   name: "App",
   components: {
     BarraLateral,
+    Formulario,
   },
 };
 </script>
@@ -15,7 +17,9 @@ export default {
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <Formulario />
+    </div>
   </main>
 </template>
 
