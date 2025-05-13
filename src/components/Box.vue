5f319a702c6a40e9d908a5f5d8920d7adecb184c
renawmontanari <regiangelica@gmail.com>
2025-05-13 19:55:46 -0300

    estilos e objetos

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,17 +1,18 @@
 <script lang="ts">
 export default {
   name: "Box",
+  data() {
+    return {
+      estilos: {
+        backgroundColor: "#636363",
+      },
+    };
+  },
 };
 </script>
 
 <template>
-  <div class="box has-text-weight-bold">
+  <div class="box has-text-weight-bold" :style="estilos">
     <slot />
   </div>
 </template>
-
-<style scoped>
-.box {
-  background-color: rgb(82, 82, 82);
-}
-</style>
