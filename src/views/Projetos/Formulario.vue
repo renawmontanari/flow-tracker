1a4840786e14966ed653af2f6ba89aa3632a8a48
renawmontanari <regiangelica@gmail.com>
2025-05-22 19:31:06 -0300

    aninhando as rotas do projeto

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -45,8 +45,7 @@ export default {
 </script>
 
 <template>
-  <section class="projetos">
-    <h1 class="title">Projetos</h1>
+  <section>
     <form @submit.prevent="salvar">
       <div class="field">
         <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
@@ -63,9 +62,3 @@ export default {
     </form>
   </section>
 </template>
-
-<style scoped>
-.projetos {
-  padding: 1.25rem;
-}
-</style>
---
+++ src/views/Projetos/Lista.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { useStore } from "@/store";
+import { computed } from "vue";
+
+export default {
+  name: "Lista",
+  setup() {
+    const store = useStore();
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
+  },
+};
+</script>
+
+<template>
+  <section>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
