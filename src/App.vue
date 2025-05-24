a4a3ad6283e78ada280ea2da28cc665a1bc6badb
renawmontanari <regiangelica@gmail.com>
2025-05-24 11:27:40 -0300

    crinado o componente de notificacoes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import BarraLateral from "./components/BarraLateral.vue";
+import Notificacoes from "./components/Notificacoes.vue";
 
 export default {
   name: "App",
   components: {
     BarraLateral,
+    Notificacoes,
   },
   data() {
     return {
@@ -28,6 +30,7 @@ export default {
       <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
+      <Notificacoes />
       <router-view></router-view>
     </div>
   </main>
---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+export default {
+  name: "Notificacoes",
+};
+</script>
+
+<template>
+  <div class="notificacoes">
+    <article class="message is-success">
+      <div class="message-header">Atenção!</div>
+      <div class="message-body">Aqui o texto de notificação!</div>
+    </article>
+    <article class="message is-warning">
+      <div class="message-header">Atenção!</div>
+      <div class="message-body">Aqui o texto de notificação!</div>
+    </article>
+    <article class="message is-danger">
+      <div class="message-header">Atenção!</div>
+      <div class="message-body">Aqui o texto de notificação!</div>
+    </article>
+  </div>
+</template>
+
+<style scoped>
+.notificacoes {
+  position: absolute;
+  right: 0;
+  width: 300px;
+  padding: 8px;
+  z-index: 105;
+}
+</style>
