4c0c9064fbb9e819c428a5a17bcb535544fac4cb
renawmontanari <regiangelica@gmail.com>
2025-05-26 21:48:59 -0300

    computed props funcionalidade para mostrar o nome do projeto

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -30,8 +30,11 @@ export default {
 <template>
   <Box>
     <div class="columns is-vcentered">
-      <div class="column is-7">
+      <div class="column is-4">
         {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
+      <div class="column is-3">
+        {{ tarefa.projeto?.nome || "Sem projeto vinculado" }}
       </div>
       <div class="column">
         <div class="is-flex is-align-items-center">
